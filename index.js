c02e0042b75d6156f9b9cd66cba3626d0b1c7802
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-01-09 13:22:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction()
 {
-  return string.funkyFunction
+  return string.funkyFunction()
 }
 
 
